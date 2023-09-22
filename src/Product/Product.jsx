526d29c95d3e846a252b4ae8a856776d7a60f978
Hassan Md. Sharfuddin Miraz <48539452+hmsmiraz@users.noreply.github.com>
2023-09-23 00:52:22 +0600

    Update Product.jsx

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Product = ({ product }) => {
   const { id, title, price, images } = product;
